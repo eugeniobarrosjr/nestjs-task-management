a5babd6de190f91de4fe3a784cba334d4bb3e409
Eugenio Barros <carlos.barros@olist.com>
2021-08-27 21:53:50 -0300

    feat: add service

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,7 @@
+import { TasksService } from './tasks.service';
+import { Controller } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+}
